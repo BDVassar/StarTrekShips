b84bf88e7fca3b9e9289c727f77f9bdb8ba5855a
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-18 14:56:48 -0700

    env.js auth0 added

--- StarTrekShips.client/src/env.js
+++ StarTrekShips.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = 'https://localhost:7045'
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'bryan-vassar.us.auth0.com'
+export const clientId = 'wbVaUZk4wdIqMXWLCnNUKKznQbywQkzf'
+export const audience = 'https://bryanclassroom.com'
