1a95ced9392723f7058235b5f885d5e1d19af082
Bryan Vassar <vassar.bryan@gmail.com>
2024-08-01 23:03:15 -0600

    updated dbSetup and added more attributes to Ship.cs model

--- StarTrekShips/Models/Ship.cs
+++ StarTrekShips/Models/Ship.cs
@@ -4,14 +4,25 @@ namespace StarTrekShips.Models;
 public class Ship
 {
   public string name { get; set; }
+  public string type { get; set; }
   public string quadrant { get; set; }
+  public string captain { get; set; }
   public int crewComplement { get; set; }
+  public int passengerCapcity { get; set; }
+  public int speed { get; set; }
   public bool phasers { get; set; }
   public bool torpedos { get; set; }
   public bool shields { get; set; }
+  public bool sensors { get; set; }
+  public bool dockingBay { get; set; }
+  public bool sickBay { get; set; }
+  public bool cloakingDevice { get; set; }
+  public bool tractorBeam { get; set; }
+  public bool transporter { get; set; }
   public string shipPrefix { get; set; }
   public string alliance { get; set; }
   public int shuttles { get; set; }
   public string classification { get; set; }
+  public int capacity { get; set; }
   public Account Creator { get; set; }
 }
--- StarTrekShips/dbSetup.sql
+++ StarTrekShips/dbSetup.sql
@@ -1,34 +1,31 @@
-CREATE TABLE
-    IF NOT EXISTS accounts(
-        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-        name varchar(255) COMMENT 'User Name',
-        email varchar(255) COMMENT 'User Email',
-        picture varchar(255) COMMENT 'User Picture'
-    ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
+) default charset utf8 COMMENT '';
 
-CREATE TABLE
-    ships(
-        id int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'primary key',
-        name VARCHAR(255) NOT NULL COMMENT 'Name of Ship',
-        quadrant VARCHAR(255) NOT NULL COMMENT 'Quadrent location',
-        crewComplement INT NOT NULL COMMENT 'Number of Crewmen',
-        phasers BOOLEAN NOT NULL COMMENT 'Does the ship have phasers',
-        torpedos BOOLEAN NOT NULL COMMENT ' Does the ship have torpedos',
-        shields BOOLEAN NOT NULL COMMENT 'Does the ship have shields',
-        shipPrefix VARCHAR(255) NOT NULL COMMENT 'Ship Prefix',
-        alliance VARCHAR(255) NOT NULL COMMENT 'Alliance',
-        shuttles INT NOT NULL COMMENT 'Does the ship have shuttles',
-        classification VARCHAR(255) NOT NULL COMMENT 'Class of ship',
-        creatorId VARCHAR(255) NOT NULL,
-        FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
-    ) default charset utf8 COMMENT '';
+CREATE TABLE ships (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'primary key',
+    name VARCHAR(255) NOT NULL COMMENT 'Name of Ship',
+    quadrant VARCHAR(255) NOT NULL COMMENT 'Quadrent location',
+    crewComplement INT NOT NULL COMMENT 'Number of Crewmen',
+    phasers BOOLEAN NOT NULL COMMENT 'Does the ship have phasers',
+    torpedos BOOLEAN NOT NULL COMMENT ' Does the ship have torpedos',
+    shields BOOLEAN NOT NULL COMMENT 'Does the ship have shields',
+    shipPrefix VARCHAR(255) NOT NULL COMMENT 'Ship Prefix',
+    alliance VARCHAR(255) NOT NULL COMMENT 'Alliance',
+    shuttles INT NOT NULL COMMENT 'Does the ship have shuttles',
+    classification VARCHAR(255) NOT NULL COMMENT 'Class of ship',
+    creatorId VARCHAR(255) NOT NULL
+) default charset utf8 COMMENT '';
 
 DROP TABLE ships;
 
 INSERT INTO
-    ships(
+    ships (
         name,
         quadrant,
         crewComplement,
