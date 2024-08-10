aa9b45f8074336359a3605dd0aa7fbea7d806438
Bryan Vassar <vassar.bryan@gmail.com>
2024-08-09 23:17:08 -0600

    updated click game

--- StarTrekShips.client/src/components/ResourceGame.vue
+++ StarTrekShips.client/src/components/ResourceGame.vue
@@ -5,6 +5,22 @@
         <div id="background-1"></div>
         <div id="background-2"></div>
         <img class="clickable-image img-fluid" :src="imageUrl" :alt="clickableObject" @click="handleClick" />
+        <!-- Addition of Quantum Generator icon -->
+        <img v-if="quantumGeneratorLevel > 0" class="clickable-image img-fluid quantum-generator"
+          :src="quantumGeneratorImageUrl" :alt="quantumGeneratorAlt" @click="buyQuantumGeneratorClick" />
+      </div>
+      <!-- Addition of resource collector components -->
+      <div class="resource-collectors col-md-4" v-if="warpCoreLevel > 0">
+        <div>
+          <img class="collector-image img-fluid" :src="antimatterSynthesizerImageUrl" :alt="antimatterSynthesizerAlt" />
+          <h4>Antimatter Synthesizer</h4>
+          <h6>+{{ warpCoreMultiplier }} Antimatter per second</h6>
+        </div>
+        <div>
+          <img class="collector-image img-fluid" :src="warpCoreStabilizerImageUrl" :alt="warpCoreStabilizerAlt" />
+          <h4>Warp Core Stabilizer</h4>
+          <h6>+{{ warpCoreStabilizerAmount }} Antimatter per second</h6>
+        </div>
       </div>
     </div>
     <div class="row justify-content-center align-items-center">
@@ -12,11 +28,15 @@
         <h3>Antimatter: {{ resources }}</h3>
         <div class="btn-group" role="group" aria-label="Basic example">
           <button class="btn btn-secondary" @click="resetGame">Reset Game</button>
-          <button class="btn btn-secondary" :disabled="warpCoreUpgradeCost > resources" @click="buyWarpCoreUpgrade">
-            Warp Core Upgrade
-          </button>
+          <button class="btn btn-secondary" :disabled="warpCoreUpgradeCost > resources" @click="buyWarpCoreUpgrade">Warp
+            Core Upgrade</button>
+          <!-- Addition of Quantum Generator upgrade button -->
+          <button class="btn btn-secondary" :disabled="quantumGeneratorUpgradeCost > resources"
+            @click="buyQuantumGeneratorUpgrade">Quantum Generator Upgrade</button>
         </div>
         <h4>{{ warpCoreUpgradeStatus }}</h4>
+        <!-- Addition of Quantum Generator status -->
+        <h4>Quantum Generator: Level {{ quantumGeneratorLevel }} | Cost: {{ quantumGeneratorUpgradeCost }}</h4>
       </div>
     </div>
   </main>
@@ -30,9 +50,18 @@ export default {
       warpCoreUpgradeCost: 100,
       warpCoreLevel: 0,
       warpCoreMultiplier: 1,
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/NGC7293_%282004%29.jpg/290px-NGC7293_%282004%29.jpg",
+      antimatterSynthesizerAmount: 1, // Amount of resources collected per second by the Antimatter Synthesizer
+      warpCoreStabilizerAmount: 5, // Amount of resources collected per second by the Warp Core Stabilizer
+      quantumGeneratorLevel: 0,
+      quantumGeneratorUpgradeCost: 500,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/NGC7293_%282004%29.jpg/290px-NGC7293_%282004%29.jpg",
       clickableObject: "Clickable Object",
+      quantumGeneratorImageUrl: "https://image.flaticon.com/icons/svg/149/149852.svg", // Replace with your desired image URL
+      quantumGeneratorAlt: "Quantum Generator",
+      antimatterSynthesizerImageUrl: "https://image.flaticon.com/icons/svg/860/860020.svg", // Replace with your desired image URL
+      antimatterSynthesizerAlt: "Antimatter Synthesizer",
+      warpCoreStabilizerImageUrl: "https://image.flaticon.com/icons/svg/125/125857.svg", // Replace with your desired image URL
+      warpCoreStabilizerAlt: "Warp Core Stabilizer",
     };
   },
   mounted() {
@@ -61,6 +90,17 @@ export default {
         this.warpCoreUpgradeCost *= 2;
       }
     },
+    buyQuantumGeneratorUpgrade() {
+      if (this.resources >= this.quantumGeneratorUpgradeCost) {
+        this.resources -= this.quantumGeneratorUpgradeCost;
+        this.quantumGeneratorLevel++;
+        this.quantumGeneratorUpgradeCost *= 2;
+      }
+    },
+    buyQuantumGeneratorClick() {
+      // Handle click event for the Quantum Generator
+      // You can add any specific functionality here, such as resource production
+    },
   },
   computed: {
     warpCoreUpgradeStatus() {
@@ -71,9 +111,30 @@ export default {
 </script>
 
 <style>
+.resource-collectors {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.collector-image {
+  width: 100px;
+  height: 100px;
+  margin-bottom: 10px;
+}
+
+/* Adjustments to centering */
 #game-container {
-  height: 100vh;
-  position: relative;
+  height: 80vh;
+}
+
+.col-md-8 {
+  max-width: 550px;
+  /* Adjust this value to fit your design */
 }
 
 #background-1 {
